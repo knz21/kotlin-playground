ce09773ad881a8a271387ae91920e80713fa1cf9
knz21 <cismollre@gmail.com>
2024-03-17 14:02:06 +0900

    random enum

--- src/main/kotlin/com/knz21/Main.kt
+++ src/main/kotlin/com/knz21/Main.kt
@@ -2,8 +2,10 @@ package com.knz21
 
 import com.knz21.snippets.FirstFromEmptyList
 import com.knz21.snippets.MatchEmptyString
+import com.knz21.snippets.RandomEnum
 
 fun main() {
     MatchEmptyString()
     FirstFromEmptyList()
+    RandomEnum()
 }
---
+++ src/main/kotlin/com/knz21/snippets/RandomEnum.kt
@@ -0,0 +1,19 @@
+package com.knz21.snippets
+
+object RandomEnum {
+
+    private const val COUNT = 1000000
+
+    operator fun invoke() {
+        val list = List(COUNT) { Three.entries.random() }
+        println("count: $COUNT\n" +
+            "${Three.A}: ${list.count { it == Three.A }}\n" +
+            "${Three.B}: ${list.count { it == Three.B }}\n" +
+            "${Three.C}: ${list.count { it == Three.C }}"
+        )
+    }
+
+    enum class Three {
+        A, B, C
+    }
+}

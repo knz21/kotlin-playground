26ad7e9e9afe58b15f32540cdd0345489c203dbd
knz21 <cismollre@gmail.com>
2024-03-23 14:51:39 +0900

    startsWith, endsWith, indexOf

--- src/main/kotlin/com/knz21/snippets/MatchEmptyString.kt
+++ src/main/kotlin/com/knz21/snippets/MatchEmptyString.kt
@@ -4,5 +4,8 @@ object MatchEmptyString : Snippet {
 
     override operator fun invoke() {
         println("Empty string is matched: ${"string".contains("")}")
+        println("startsWith: ${"string".startsWith("")}")
+        println("endsWith: ${"string".endsWith("")}")
+        println("indexOf: ${"string".indexOf("")}")
     }
 }

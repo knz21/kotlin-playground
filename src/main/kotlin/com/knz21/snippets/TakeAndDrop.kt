edf15f5c2f4f4c9558909dbe89a1a6e9f2628924
knz21 <cismollre@gmail.com>
2024-03-23 15:04:13 +0900

    last

--- src/main/kotlin/com/knz21/snippets/TakeAndDrop.kt
+++ src/main/kotlin/com/knz21/snippets/TakeAndDrop.kt
@@ -9,5 +9,9 @@ object TakeAndDrop: Snippet {
         println("drop(3): ${list.drop(3)}")
         println("take(10): ${list.take(10)}")
         println("drop(10): ${list.drop(10)}")
+        println("takeLast(3): ${list.takeLast(3)}")
+        println("dropLast(3): ${list.dropLast(3)}")
+        println("takeLast(10): ${list.takeLast(10)}")
+        println("dropLast(10): ${list.dropLast(10)}")
     }
 }
